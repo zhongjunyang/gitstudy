cd07471fdb17371c08a7455e3e536d18e65a0de0
钟君扬 <2956435239@qq.com>
2022-01-24 20:05:42 +0800

    第9次提交信息

--- src/main/java/com/zhong/gitstudy/HelloWorld.java
+++ src/main/java/com/zhong/gitstudy/HelloWorld.java
@@ -3,5 +3,7 @@ package com.zhong.gitstudy;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, My World!");
+        System.out.println("Hello, My World!");
+        System.out.println("Hello, My World!");
     }
 }
